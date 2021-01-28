262501e2e31a9bc6e8d7cc073ff2d2f841a386ca
thomasyu888 <thomas.yu@sagebase.org>
2021-01-28 20:17:42 +0800

    update example

--- example.py
+++ example.py
@@ -1,19 +1,23 @@
+"""Example code to interface with python sdk"""
 import synclient
 
 configuration = synclient.Configuration(
     host="https://repo-prod.prod.sagebase.org/repo/v1",
 )
-# Obtain PAT from synapse under settings
+# Obtain PAT from synapse by going to "settings"
 pat = ''
 configuration.access_token = pat
-
 
 with synclient.ApiClient(configuration) as api_client:
     # Create an instance of the API class
     api_instance = synclient.ChallengeServicesApi(api_client)
+    user_service = synclient.UserProfileServicesApi(api_client)
 
-challenge = api_instance.get_challenge(2797)
+# obtain own user profile
+userprofile = user_service.get_my_own_user_profile()
 
-challenge_teams = api_instance.list_challenge_teams(2797)
-participants = api_instance.list_participants_in_challenge(2797)
-challenges = api_instance.list_challenges_for_participant(3324230)
+# Example challenge services
+challenge = api_instance.get_challenge(4328)
+challenge_teams = api_instance.list_challenge_teams(4328)
+participants = api_instance.list_participants_in_challenge(4328)
+challenges = api_instance.list_challenges_for_participant(userprofile.owner_id)
