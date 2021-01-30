39aa9a9b9b13ca2101af8ad4fc0a3945703ad6d6
thomasyu888 <thomas.yu@sagebase.org>
2021-01-30 23:31:10 +0800

    Update examples

--- example.py
+++ example.py
@@ -1,5 +1,9 @@
 """Example code to interface with python sdk"""
 import synclient
+from synclient.api import challenge_services_api
+from synclient.api import user_profile_services_api
+from synclient.api import entity_services_api
+
 
 configuration = synclient.Configuration(
     host="https://repo-prod.prod.sagebase.org/repo/v1",
@@ -10,14 +14,14 @@ configuration.access_token = pat
 
 with synclient.ApiClient(configuration) as api_client:
     # Create an instance of the API class
-    api_instance = synclient.ChallengeServicesApi(api_client)
-    user_service = synclient.UserProfileServicesApi(api_client)
+    api_instance = challenge_services_api.ChallengeServicesApi(api_client)
+    user_service = user_profile_services_api.UserProfileServicesApi(api_client)
+    entity_service = entity_services_api.EntityServicesApi(api_client)
+    # obtain own user profile
+    userprofile = user_service.get_my_own_user_profile()
 
-# obtain own user profile
-userprofile = user_service.get_my_own_user_profile()
-
-# Example challenge services
-challenge = api_instance.get_challenge(4328)
-challenge_teams = api_instance.list_challenge_teams(4328)
-participants = api_instance.list_participants_in_challenge(4328)
-challenges = api_instance.list_challenges_for_participant(userprofile.owner_id)
+    # Example challenge services
+    challenge = api_instance.get_challenge(4328)
+    challenge_teams = api_instance.list_challenge_teams(4328)
+    participants = api_instance.list_participants_in_challenge(4328)
+    challenges = api_instance.list_challenges_for_participant(userprofile.owner_id)
